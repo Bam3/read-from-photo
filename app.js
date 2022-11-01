bfb2af7c5e1c31ef74195974d5ff674a3c50aca9
Miha Bastašič <bastasic.miha@gmail.com>
2022-11-01 16:22:45 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-Tesseract.recognize("/photos/IMG_20221031_212502.jpg", "slv", {
-  logger: (m) => console.log(m),
-}).then(({ data: { text } }) => {
-  console.log(text);
+const { createWorker } = require("tesseract.js");
+
+const worker = createWorker({
+  logger: (m) => console.log(m), // Add logger here
 });
+
+(async () => {
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize("eng");
+  const {
+    data: { text },
+  } = await worker.recognize(
+    "https://tesseract.projectnaptha.com/img/eng_bw.png"
+  );
+  console.log(text);
+  await worker.terminate();
+})();
