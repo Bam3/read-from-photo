1615d4afcd965e51f7833048eab46be7438f7e38
Miha Bastašič <bastasic.miha@gmail.com>
2022-11-02 21:09:25 +0100

    back camera

--- takePhoto.js
+++ takePhoto.js
@@ -19,7 +19,7 @@ function startup() {
 startup();
 const constraints = {
   audio: false,
-  video: { width: 1280, height: 720, facingMode: "user" },
+  video: { width: 1280, height: 720, facingMode: "environment" },
 };
 navigator.mediaDevices
   .getUserMedia(constraints)
